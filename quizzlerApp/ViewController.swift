e2bca1a133f7c9a128f7ccf5ddfa5725c1799eda
Andres <carlos.camyoh@gmail.com>
2022-06-25 20:10:42 -0500

    creando los outlets y actions, cambiando nombre de botones a verdadero y falso, uninedo el mismo action a los dos botones

--- quizzlerApp/ViewController.swift
+++ quizzlerApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelPregunta: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var botonVerdadero: UIButton!
+    @IBOutlet weak var botonFalso: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelPregunta.text = "2^6 = 64"
     }
 
-
+    @IBAction func botonRespuestaPresionado(_ sender: UIButton) {
+    }
+    
 }
 
